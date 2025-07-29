8c07351460b3ea7d9956618bac2a26d53fc030ea
Arif4651 <knowldge465109@gmail.com>
2025-07-30 02:46:38 +0600

    admin managemnet for catalog done by arif

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:talehive/pages/admin/admin_dashboard.dart';
+import 'package:talehive/pages/admin/books/books_and_club_management.dart';
+import 'package:talehive/pages/admin/catalog/all_users_books_reqst_Catalog_management.dart';
+import 'package:talehive/pages/admin/users/user_management.dart';
 import 'package:talehive/pages/club/book_club.dart';
 import 'package:talehive/pages/user/author_dashboard.dart';
 import 'package:talehive/pages/user/book_details.dart';
 import 'package:talehive/pages/user/user_books.dart';
 import 'package:talehive/pages/user/user_dashboard.dart';
 import 'package:talehive/pages/user/user_home.dart';
-import 'package:talehive/user_authentication/login.dart';
+
 
 class myapp extends StatelessWidget {
   const myapp({super.key});
@@ -16,7 +19,7 @@ class myapp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
 
-        home: BookDetailsPage(bookId: 'mock',),
+        home:AllUsersBookRequestCatalogManagementPage(),
     );
   }
 }
